5620f63a6ae2771d61e909a46897775ab78eebcc
Admin <admin@Qwertys-iMac.local>
2018-11-26 11:28:08 -0800

    type: fix | added form to rember input incase of error

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -74,7 +74,7 @@
 		private function validatePasswords($pw, $pw2) {
 
 			if($pw != $pw2) {
-				array_push($this->errorArray, Constants::$passwordsDoNotMatch;
+				array_push($this->errorArray, Constants::$passwordsDoNotMatch);
 				return;
 			}
 
