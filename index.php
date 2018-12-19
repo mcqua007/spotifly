88ff3c3440e1388f433e0f43395c888e12dd7f6b
Admin <admin@Qwertys-iMac.local>
2018-12-19 15:59:45 -0800

    finished the registration, sessions

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+		include("includes/config.php");
+		
+		//used to logout
+		//session_destroy(;)
+
+		if(isset($_SESSION['userLoggedIn'])){
+			$userLoggedIn = $_SESSION['userLoggedIn'];
+		}
+		else {
+			header("Location: register.php");
+		}
+ ?>
+
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
