ea45db394c0b52fb675842840390b83fdbac256a
Admin <admin@Qwertys-iMac.local>
2018-12-28 13:11:34 -0800

    type: feat | added the playback par sliding effect, to choose where to start song or to jump too

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var currentPlaylist = [];
 var audioElement;
-
+var mouseDown = false;
 
 function formatTime(seconds) {
 	var time = Math.round(seconds);
@@ -48,6 +48,9 @@ function Audio() {
 
 	this.pause = function() {
 		this.audio.pause();
+	}
+	this.setTime = function(seconds){
+		this.audio.currentTime = seconds;
 	}
 
 }
