79751431dea35a5afa7419c9d6fc7755b79bcf48
Admin <sean@decrypteddesign.com>
2019-01-18 16:43:42 -0800

    type: feat |added settings page

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -16,6 +16,12 @@
       return $this->username;
     }
 
+		public function getFirstAndLastName(){
+			$query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as name FROM users WHERE username='$this->username'");
+			$row = mysqli_fetch_array($query);
+			return $row['name'];
+		}
+
   }
 
   ?>
