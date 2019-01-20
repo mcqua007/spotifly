bb7de207310131f2f9c7d15544a5d6d53aedf3b7
Admin <sean@decrypteddesign.com>
2019-01-19 18:47:37 -0800

    type: feat | started update password funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,17 @@ function updateEmail(emailClass){
 		$("." + emailClass).nextAll(".message").text(response);
 	});
 }
+
+function updatePassword(oldPassword, newPasswordClass1, newPasswordClass2){
+	var oldPassword = $("." + oldPasswordClass).val();
+	var newPassword1 = $("." + newPasswordClass1).val();
+	var newPassword2 = $("." + newPasswordClass2).val();
+
+	$.post("includes/handlers/ajax/updatePassword.php", {oldPassword : oldPassword, newPassword1: newPassword1, newPAssword2: newPAssword2, username: userLoggedIn}).done(function(response){
+		$("." + oldPasswordClass).nextAll(".message").text(response);
+	});
+}
+
 function logout(){
 	$.post("includes/handlers/ajax/logout.php", function(){
 			location.reload(); //force reloads current page
--- includes/handlers/ajax/updateEmail.php
+++ includes/handlers/ajax/updateEmail.php
@@ -1,30 +1,33 @@
 <?php
 include("../../config.php");
-  if(!isset($_POST['username'])){
-    echo "ERROR: Could not set username!";
-    exit();
-  }
-  if(isset($_POST['email']) && $_POST['email'] != ""){
-      $usernmae  = $_POST['username'];
-      $email = $_POST['email'];
 
-      if(!filter_var($email, FILTER_VALIDATE_EMAIL{
-      echo "Email is invalid!";
-       exit();
+if(!isset($_POST['username'])) {
+	echo "ERROR: Could not set username";
+	exit();
+}
 
+if(isset($_POST['email']) && $_POST['email'] != "") {
 
-      $emailCheck = mysqli_query($con, "SELECT email FROM users WHERE email='$email' AND username != '$username'");//check s if email has in use by everyone but the current user
+	$username = $_POST['username'];
+	$email = $_POST['email'];
 
-      if(mysqli_num_rows($emailCheck > 0)){
-        echo "Email is already in user";
-        exit();
-      }
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		echo "Email is invalid";
+		exit();
+	}
 
-      $updateQuery = mysqli_wuery($con, "UPDATE users SET EMAIL '$email' WHERE username = '$username'");
-      echo "Update Succesful!";
-  }
-  else{
-    echo "You must provide a email!";
-  }
+	$emailCheck = mysqli_query($con, "SELECT email FROM users WHERE email='$email' AND username != '$username'");
+	if(mysqli_num_rows($emailCheck) > 0) {
+		echo "Email is already in use";
+		exit();
+	}
+
+	$updateQuery = mysqli_query($con, "UPDATE users SET email = '$email' WHERE username='$username'");
+	echo "Update successful";
+
+}
+else {
+	echo "You must provide an email";
+}
 
 ?>
