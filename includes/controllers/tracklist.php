7107037303991765fad85ffb474e56aeb15a2d78
Admin <sean@decrypteddesign.com>
2019-01-27 17:59:55 -0800

    type: fix | added the total song likes to be loaded on page laod

--- includes/controllers/tracklist.php
+++ includes/controllers/tracklist.php
@@ -55,6 +55,7 @@ while($row = mysqli_fetch_array($likedQuery)) {
 					</div>
 					<div class='totalLikes'>
 						<div class='total-like-text-". $albumSong->getId() . "'>
+						". $albumSong->getNumberOfLikes() . "
 						</div>
 					</div>
 
